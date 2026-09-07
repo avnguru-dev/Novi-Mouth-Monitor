c04cefa3cfd11dd282a92f186e487098dccfc33c
avnguru-dev <avnguru@nuevaschool.org>
2026-09-07 09:00:27 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,8 +30,9 @@ let predictionConfidence = 0;
 let currentState = -1;
 let stateStartTime = 0;
 
-let stateCounts;
-let stateTimes;
+let stateCounts = [0, 0, 0, 0, 0];
+let stateTimes = [0, 0, 0, 0, 0];
+
 
 let sessionStartTime = 0;
 let activePage = "camera";
