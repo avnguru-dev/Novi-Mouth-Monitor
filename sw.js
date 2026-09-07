0df8bffbc91384b459e646f460ac122d152caf65
avnguru-dev <avnguru@nuevaschool.org>
2026-09-07 09:22:59 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
 self.addEventListener("install", function(event) {
-  console.log("Notification service worker installed.");
   self.skipWaiting();
 });
 
 self.addEventListener("activate", function(event) {
-  console.log("Notification service worker activated.");
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    self.clients.claim()
+  );
 });
 
 self.addEventListener("message", function(event) {
 
   if (event.data === "MOUTH_OPEN_ALERT") {
 
-    self.registration.showNotification("Posture Alert!", {
-      body: "Your mouth has been open for too long.",
-      requireInteraction: false,
-      tag: "mouth-open-alert"
-    });
+    self.registration.showNotification(
+      "Posture Alert!",
+      {
+        body: "Your mouth has been open for too long.",
+        requireInteraction: false,
+        tag: "mouth-open-alert"
+      }
+    );
 
   }
 
