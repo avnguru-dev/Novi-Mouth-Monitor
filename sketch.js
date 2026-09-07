fdca2929d984826834cde593f903e763bdeafdcf
avnguru-dev <avnguru@nuevaschool.org>
2026-09-07 09:01:56 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,8 +30,9 @@ let predictionConfidence = 0;
 let currentState = -1;
 let stateStartTime = 0;
 
-let stateCounts = [0, 0, 0, 0, 0];
-let stateTimes = [0, 0, 0, 0, 0];
+let stateCounts =;
+let stateTimes =;
+
 
 
 let sessionStartTime = 0;
